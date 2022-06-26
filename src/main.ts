270891d513bd7125d4686886e06b68fbfa7d68c9
Hemin Johnson <achayan1992@gmail.com>
2022-06-26 17:55:03 +0200

    feat: add pagination

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      forbidNonWhitelisted: true,
-      transform: true,
       whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
   await app.listen(3000);
