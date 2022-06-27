a07f7b4cc7e5cd0f71802c8672f6a13e26e61cb3
Hemin Johnson <achayan1992@gmail.com>
2022-06-27 17:23:20 +0200

    feat: add custom provider pattern

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,11 +5,18 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    {
+      provide: COFFEE_BRANDS, // 👈
+      useValue: ['buddy brew', 'nescafe'], // array of coffee brands,
+    },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
